09751ad3a95ada83472540ebaa3ad1807d918760
HyeSoo Park <flyjwayur@gmail.com>
2020-05-01 11:07:27 +0300

    Part1.4 : Modify parts data to array and pass them to components as props

--- Part1/courseInfo/courses/src/components/Total.js
+++ Part1/courseInfo/courses/src/components/Total.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-const total = ({ totalExercise }) => {
-  return <p>Number of exercises {totalExercise}</p>;
+const total = ({ parts }) => {
+  const part0 = parts[0];
+  const part1 = parts[1];
+  const part2 = parts[2];
+
+  return (
+    <p>
+      Number of exercises:
+      {part0.exercises + part1.exercises + part2.exercises}
+    </p>
+  );
 };
 
 export default total;
--- Part1/courseInfo/courses/src/index.js
+++ Part1/courseInfo/courses/src/index.js
@@ -7,28 +7,27 @@ import Total from "./components/Total";
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
 
-  const parts = [part1, part2, part3];
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course}></Header>
       <Content parts={parts}></Content>
-      <Total
-        totalExercise={part1.exercises + part2.exercises + part3.exercises}
-      ></Total>
+      <Total parts={parts}></Total>
     </div>
   );
 };
