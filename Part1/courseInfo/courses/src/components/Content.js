91eea2a936615e1200553aa6a23ab899868f8e65
HyeSoo Park <flyjwayur@gmail.com>
2020-05-01 09:54:40 +0300

    Part1.2 : Refactor the content component and add part component

--- Part1/courseInfo/courses/src/components/Content.js
+++ Part1/courseInfo/courses/src/components/Content.js
@@ -1,13 +1,14 @@
 import React from "react";
+import Part from "./Part";
 
 const Content = ({ parts }) => {
   return (
     <div>
-      {parts.map((part) => (
-        <p>
-          {part.part} {part.count}
-        </p>
-      ))}
+      {parts
+        ? parts.map(({ name, count }, index) => (
+            <Part key={"_" + index} name={name} count={count} />
+          ))
+        : null}
     </div>
   );
 };
---
+++ Part1/courseInfo/courses/src/components/Part.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Part = ({ name, count }) => {
+  return (
+    <p>
+      {name} {count}
+    </p>
+  );
+};
+
+export default Part;
