3d1198d0e4f0c3af5bc78867895dfc7368e35119
HyeSoo Park <flyjwayur@gmail.com>
2020-05-16 11:55:28 +0300

    Part1.12: anecdotes step1

---
+++ Part1/anecdotes/src/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const anecdotes = [
+  "If it hurts, do it more often",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Premature optimization is the root of all evil.",
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+];
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0);
+
+  const getRandomInt = (max) => {
+    return Math.floor(Math.random() * max);
+  };
+
+  const handleNextAnecdote = (anecdotesLength) => () => {
+    setSelected(getRandomInt(anecdotesLength));
+  };
+
+  return (
+    <div>
+      <div>{props.anecdotes[selected]}</div>
+      <button onClick={handleNextAnecdote(anecdotes.length)}>
+        Next anecdote
+      </button>
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App anecdotes={anecdotes} />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
