0394fc2b046769cdc7d37c9d06f3c318c61424b4
HyeSoo Park <flyjwayur@gmail.com>
2020-05-16 11:16:03 +0300

    Part1.9 : unicafe step4

--- Part1/unicafe/src/components/Statistics.js
+++ Part1/unicafe/src/components/Statistics.js
@@ -3,17 +3,23 @@ import Heading from "./Heading";
 
 const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad;
-  const average = all !== 0 ? good - bad / all : 0;
-  const positive = all !== 0 ? (good / all) * 100 : 0;
+  const average = good - bad / all;
+  const positive = (good / all) * 100;
   return (
     <div>
       <Heading name="statistics" />
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>all {all}</div>
-      <div>average {average}</div>
-      <div>positive Feedback {positive + "%"}</div>
+      {all !== 0 ? (
+        <div>
+          <div>good {good}</div>
+          <div>neutral {neutral}</div>
+          <div>bad {bad}</div>
+          <div>all {all}</div>
+          <div>average {average}</div>
+          <div>positive Feedback {positive + "%"}</div>
+        </div>
+      ) : (
+        "No feedback given"
+      )}
     </div>
   );
 };
